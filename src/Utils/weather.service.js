75a735fc7e180ff50fdd97378e55510c368d324d
GaianiMagali <magaliantonella.gaiani@gmail.com>
2020-07-30 00:45:18 -0300

    Se agrego la ciudad y la provincia

--- src/Utils/weather.service.js
+++ src/Utils/weather.service.js
@@ -1,10 +1,20 @@
 import axios from 'axios';
 
 class WeatherService {
+    constructor() {
+        this.latitud = -34.6507;
+        this.longitud = -58.6222034;
+    }
 
     async getWeather() {
-        const response = await axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=-34.5708&lon=-58.6243&exclude=current,hourly&appid=b9eec6d7a7247508bc1aeec9bc2dee6c&units=metric")
-        
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${this.latitud}&lon=${this.longitud}&exclude=current,hourly&appid=b9eec6d7a7247508bc1aeec9bc2dee6c&units=metric`)
+
+        return response;
+    }
+
+    async getApidataCloud() {
+        const response = await axios.get(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${this.latitud}&longitude=${this.longitud}&localityLanguage=en`)
+
         return response;
     }
 }
